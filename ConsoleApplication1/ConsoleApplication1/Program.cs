40bde132bf59592920f8edc671f29a2c9941d893
pausavaseree <panuausavasereelert@agoda.com>
2015-04-11 18:05:11 +0700

    First commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array
+            //int[] studentList;
+            //studentList = new int[];
+            //studentList[0] = 9;
+            //studentList[1] = 8;
+
+            //ArrayList studentList = new ArrayList();
+            //studentList.Add("first");
+            //studentList.Add("second");
+
+            //for (var i = 0; i < studentList.Count; i++)
+            //{
+            //    Trace.WriteLine(studentList[i]);
+            //}
+
+            //foreach (var j in studentList)
+            //{
+            //    Trace.WriteLine(j);
+            //}
+
+            //int [,] multiArray = new int[2,3] {{1,2,3},{4,5,6}};
+            //Trace.WriteLine(multiArray[0,1]);
+
+            //string MANIPULATION
+            //string input = "Hello how Are you";
+            //string reverseStr = "";
+            //string[] words = input.Split(' ');
+            //int index = 0;
+
+            //foreach (var w in words)
+            //{
+            //    for (var i = w.Length - 1; i >= 0; i--)
+            //    {
+            //        reverseStr = reverseStr + w[i];
+            //    }
+            //    if (index != w.Length)
+            //    {
+            //        reverseStr += " ";
+            //    }
+            //    index++;
+            //}
+            //Trace.WriteLine(reverseStr);
+
+            string input = "Hello how Are you";
+            string[] words = input.Split(' ');
+            string output = "";
+            for(var i=words.Length - 1; i >= 0 ;i--)
+            {
+                output += words[i];
+                if (i != 0)
+                {
+                    output += " ";
+                }
+            }
+            Trace.WriteLine(output);
+        }
+    }
+}
---
+++ HashMap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HashMap
+{
+    //private int ind;
+    //private string val;
+
+	public HashMap()
+	{
+	}
+
+    //public int Ind
+    //{
+    //    get;
+    //    set;
+    //}
+
+    public string[] Val
+    {
+        get;
+        set;
+    }
+
+    public void Add(int index, string assignedVal)
+    {
+        this.Val[index] = assignedVal;
+    }
+
+
+    public static void Main()
+    {
+        HashMap hashMap = new HashMap();
+        hashMap.Add(0, "test");
+        //hashMap.Ind = 1;
+        //hashMap.Val = "Test";
+        Console.WriteLine("{0}, {1}", hashMap.Val, hashMap.Val);
+        //Hashtable hashtable = new Hashtable();
+        //hashtable[1] = "One";
+        //hashtable[2] = "Two";
+        //hashtable[13] = "Thirteen";
+
+        //System.Console.WriteLine("Hello, World!");
+        //foreach (DictionaryEntry entry in hashtable)
+        //{
+        //    Console.WriteLine("{0}, {1}", entry.Key, entry.Value);
+        //}
+    }
+}
